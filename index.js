d736d5e8f1e5cfd19f1458e00e064c87a0d7415b
Abdullah <abdullah@alshai5.com>
2018-11-15 13:14:43 +0300

    Added some visuals.

--- index.js
+++ index.js
@@ -1,114 +1,171 @@
-Reveal.initialize({
-    // Display controls in the bottom right corner
-    controls: true,
+(() => {
+    "use strict";
 
-    // Display a presentation progress bar
-    progress: true,
+    Reveal.initialize({
+        // Display controls in the bottom right corner
+        controls: true,
 
-    // Set default timing of 2 minutes per slide
-    defaultTiming: 120,
+        // Display a presentation progress bar
+        progress: true,
 
-    // Display the page number of the current slide
-    slideNumber: false,
+        // Set default timing of 2 minutes per slide
+        defaultTiming: 120,
 
-    // Push each slide change to the browser history
-    history: false,
+        // Display the page number of the current slide
+        slideNumber: false,
 
-    // Enable keyboard shortcuts for navigation
-    keyboard: true,
+        // Push each slide change to the browser history
+        history: false,
 
-    // Enable the slide overview mode
-    overview: true,
+        // Enable keyboard shortcuts for navigation
+        keyboard: true,
 
-    // Vertical centering of slides
-    center: true,
+        // Enable the slide overview mode
+        overview: true,
 
-    // Enables touch navigation on devices with touch input
-    touch: true,
+        // Vertical centering of slides
+        center: true,
 
-    // Loop the presentation
-    loop: false,
+        // Enables touch navigation on devices with touch input
+        touch: true,
 
-    // Change the presentation direction to be RTL
-    rtl: false,
+        // Loop the presentation
+        loop: false,
 
-    // Randomizes the order of slides each time the presentation loads
-    shuffle: false,
+        // Change the presentation direction to be RTL
+        rtl: false,
 
-    // Turns fragments on and off globally
-    fragments: true,
+        // Randomizes the order of slides each time the presentation loads
+        shuffle: false,
 
-    // Flags if the presentation is running in an embedded mode,
-    // i.e. contained within a limited portion of the screen
-    embedded: false,
+        // Turns fragments on and off globally
+        fragments: true,
 
-    // Flags if we should show a help overlay when the questionmark
-    // key is pressed
-    help: true,
+        // Flags if the presentation is running in an embedded mode,
+        // i.e. contained within a limited portion of the screen
+        embedded: false,
 
-    // Flags if speaker notes should be visible to all viewers
-    showNotes: false,
+        // Flags if we should show a help overlay when the questionmark
+        // key is pressed
+        help: true,
 
-    // Global override for autolaying embedded media (video/audio/iframe)
-    // - null: Media will only autoplay if data-autoplay is present
-    // - true: All media will autoplay, regardless of individual setting
-    // - false: No media will autoplay, regardless of individual setting
-    autoPlayMedia: null,
+        // Flags if speaker notes should be visible to all viewers
+        showNotes: false,
 
-    // Number of milliseconds between automatically proceeding to the
-    // next slide, disabled when set to 0, this value can be overwritten
-    // by using a data-autoslide attribute on your slides
-    autoSlide: 0,
+        // Global override for autolaying embedded media (video/audio/iframe)
+        // - null: Media will only autoplay if data-autoplay is present
+        // - true: All media will autoplay, regardless of individual setting
+        // - false: No media will autoplay, regardless of individual setting
+        autoPlayMedia: null,
 
-    // Stop auto-sliding after user input
-    autoSlideStoppable: true,
+        // Number of milliseconds between automatically proceeding to the
+        // next slide, disabled when set to 0, this value can be overwritten
+        // by using a data-autoslide attribute on your slides
+        autoSlide: 0,
 
-    // Use this method for navigation when auto-sliding
-    autoSlideMethod: Reveal.navigateNext,
+        // Stop auto-sliding after user input
+        autoSlideStoppable: true,
 
-    // Enable slide navigation via mouse wheel
-    mouseWheel: false,
+        // Use this method for navigation when auto-sliding
+        autoSlideMethod: Reveal.navigateNext,
 
-    // Hides the address bar on mobile devices
-    hideAddressBar: true,
+        // Enable slide navigation via mouse wheel
+        mouseWheel: false,
 
-    // Opens links in an iframe preview overlay
-    previewLinks: false,
+        // Hides the address bar on mobile devices
+        hideAddressBar: true,
 
-    // Transition style
-    transition: 'linear', // none/fade/slide/convex/concave/zoom
+        // Opens links in an iframe preview overlay
+        previewLinks: false,
 
-    // Transition speed
-    transitionSpeed: 'default', // default/fast/slow
+        // Transition style
+        transition: 'linear', // none/fade/slide/convex/concave/zoom
 
-    // Transition style for full page slide backgrounds
-    backgroundTransition: 'fade', // none/fade/slide/convex/concave/zoom
+        // Transition speed
+        transitionSpeed: 'default', // default/fast/slow
 
-    // Number of slides away from the current that are visible
-    viewDistance: 3,
+        // Transition style for full page slide backgrounds
+        backgroundTransition: 'fade', // none/fade/slide/convex/concave/zoom
 
-    // Parallax background image
-    parallaxBackgroundImage: '', // e.g. "'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg'"
+        // Number of slides away from the current that are visible
+        viewDistance: 3,
 
-    // Parallax background size
-    parallaxBackgroundSize: '', // CSS syntax, e.g. "2100px 900px"
+        // Parallax background image
+        parallaxBackgroundImage: '', // e.g. "'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg'"
 
-    // Number of pixels to move the parallax background per slide
-    // - Calculated automatically unless specified
-    // - Set to 0 to disable movement along an axis
-    parallaxBackgroundHorizontal: null,
-    parallaxBackgroundVertical: null,
+        // Parallax background size
+        parallaxBackgroundSize: '', // CSS syntax, e.g. "2100px 900px"
 
-    // The display mode that will be used to show slides
-    display: 'block',
+        // Number of pixels to move the parallax background per slide
+        // - Calculated automatically unless specified
+        // - Set to 0 to disable movement along an axis
+        parallaxBackgroundHorizontal: null,
+        parallaxBackgroundVertical: null,
 
-    math: {
-        // mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js',
-        config: 'TeX-AMS_HTML-full'
-    },
-    dependencies: [
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0/lib/js/classList.js' },
-        { src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0/plugin/math/math.js', async: true }
-    ]
-});
+        // The display mode that will be used to show slides
+        display: 'block',
 
+        math: {
+            // mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js',
+            config: 'TeX-AMS_HTML-full'
+        },
+        chart: {
+            defaults: { 
+                global: { 
+                    title: { fontColor: "#000" }, 
+                    legend: {
+                        labels: { fontColor: "#000" },
+                    },
+                },
+                scale: { 
+                    scaleLabel: { fontColor: "#000" }, 
+                    gridLines: { color: "#000", zeroLineColor: "#000" }, 
+                    ticks: { fontColor: "#000" }, 
+                } 
+            },
+            line: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ], "borderDash": [ [5,10], [0,0] ]}, 
+            bar: { backgroundColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]}, 
+            pie: { backgroundColor: [ ["rgba(220,120,120,.8)" , "rgba(20,220,220,.8)", "rgba(20,220,20,.8)", "rgba(220,220,20,.8)", "rgba(20,20,220,.8)"] ]},
+            radar: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]}, 
+        },
+        dependencies: [
+            { src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0/lib/js/classList.js' },
+            { src: 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0/plugin/math/math.js', async: true },
+            { src: 'plugins/chart/Chart.min.js' },				
+            { src: 'plugins/chart/csv2chart.js' },
+        ]
+    });
+
+    // let ctx = document.getElementById('internetUsers').getContext('2d');
+    // let chart = new Chart(ctx, {
+    //     // The type of chart we want to create
+    //     type: 'line',
+    //     // The data for our dataset
+    //     data: {
+    //         labels: ["2010", "2016"],
+    //         datasets: [{
+    //             label: "Internet Users",
+    //             backgroundColor: 'rgb(255, 99, 132)',
+    //             borderColor: 'rgb(255, 99, 132)',
+    //             data: [1.991, 3.385],
+    //         }]
+    //     },
+    //     // Configuration options go here
+    //     options: {
+    //         // responsive: true,
+    //     }
+    // });
+
+})();
+
+
+/*
+,"backgroundColor":"rgba(220,120,120,.8)"
+,"backgroundColor":"rgba(20,220,220,.8)"
+
+
+line: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ], "borderDash": [ [5,10], [0,0] ]}, 
+bar: { backgroundColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]}, 
+pie: { backgroundColor: [ ["rgba(0,0,0,.8)" , "rgba(220,20,20,.8)", "rgba(20,220,20,.8)", "rgba(220,220,20,.8)", "rgba(20,20,220,.8)"] ]},
+radar: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]}, 
+*/
